6d252c520d9ba2d467701552589051ca66306f97
Pranith Kumar K <pkarampu@redhat.com>
2013-04-22 16:31:12 +0530

    Don't print extra separators
    Signed-off-by: Pranith Kumar K <pkarampu@redhat.com>

--- scripts/build-gfid-db.sh
+++ scripts/build-gfid-db.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find $1 -path $1/.glusterfs -prune  -o -print| while read fpath; do gfid=$(getfattr -n trusted.gfid -ehex $fpath|grep trusted.gfid|cut -f2 -d'='); size=$(stat -c "%s" $fpath); echo "$fpath => $gfid $size"; done
+find $1 -path $1/.glusterfs -prune  -o -print| while read fpath; do gfid=$(getfattr -n trusted.gfid -ehex $fpath|grep trusted.gfid|cut -f2 -d'='); size=$(stat -c "%s" $fpath); echo "$fpath $gfid $size"; done
