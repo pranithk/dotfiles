853fbf4b6e4591fd2a0e3b021324ba0e0dcfac30
Pranith Kumar K <pkarampu@redhat.com>
2013-05-08 15:32:50 +0530

    Script to install gluster from source
    Signed-off-by: Pranith Kumar K <pkarampu@redhat.com>

---
+++ scripts/build_gluster.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+tar xf $1 || exit 1
+gluster_tar=${1%.*}
+gluster_dir=${gluster_tar%.*}
+
+cd $gluster_dir
+find . | xargs touch && ./autogen.sh && ./configure --enable-fusermount && make CFLAGS="-DDEBUG -g3" install -j 32 1>/dev/null
