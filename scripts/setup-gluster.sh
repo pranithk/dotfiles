6ddc533aca442e36479f4704fb4bef8815fb8ffa
Pranith Kumar K <pkarampu@redhat.com>
2013-05-14 17:28:38 +0530

    Script to setup gluster on remote hosts
    Signed-off-by: Pranith Kumar K <pkarampu@redhat.com>

---
+++ scripts/setup-gluster.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+tgz_name=$1
+gluster_tar=${1%.*}
+gluster_dir=${gluster_tar%.*}
+hostsfile=$2
+rm -f $tgz_name
+make distclean
+./autogen.sh && ./configure && make CFLAGS="-g3 -DDEBUG" install -j 32 1>/dev/null && make dist
+for host in `cat $hostsfile`
+do
+        scp $tgz_name root@$host:
+        echo "tar xzf $tgz_name && cd $gluster_dir && find . | xargs touch && ./autogen.sh && ./configure --enable-fusermount && make CFLAGS=\"-DDEBUG -g3\" install -j 32 1>/dev/null" | ssh root@$host bash -xs  &
+done
+wait
+
