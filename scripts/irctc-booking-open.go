e84ed08510fdc56c514a93d13c5cf04b654e77aa
Pranith Kumar K <pkarampu@redhat.com>
2013-06-02 13:55:18 +0530

    Program to aid in irctc planning

---
+++ scripts/irctc-booking-open.go
@@ -0,0 +1,35 @@
+//This program prints the date on which bookings are open for a specified date
+//on irctc
+
+package main
+import (
+        "os"
+        "fmt"
+        "time"
+)
+
+func gettime_or_exit (date string) (time.Time) {
+        t, err := time.Parse ("Jan 02, 2006", date)
+        if err != nil {
+                fmt.Println (err)
+                os.Exit(-1)
+        }
+        return t
+}
+
+func main() {
+        if len(os.Args) != 2 {
+                fmt.Println ("Date (Ex: \"Aug 03, 2013\") should be given as argument")
+                os.Exit(-1)
+        }
+        //According to irctc, we can book Aug 3rd 2013 ticket on June 2nd
+        //Lets figure out the duration
+        travelday := gettime_or_exit ("Aug 03, 2013")
+        bookingday := gettime_or_exit ("Jun 02, 2013")
+        timediff := travelday.Sub(bookingday)
+
+        //Subtract this duration from the input to get the booking date
+        travelday = gettime_or_exit (os.Args[1])
+        bookingday = travelday.Add (-timediff)
+        fmt.Println ("Booking for ", os.Args[1], " opens on: ", bookingday.Month(), bookingday.Day(), bookingday.Year())
+}
