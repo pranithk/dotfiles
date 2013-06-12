403453fe0955ee731721e758026265fbd2a926be
Pranith Kumar K <pkarampu@redhat.com>
2013-06-12 11:48:22 +0530

    changed booking dates

--- scripts/irctc-booking-open.go
+++ scripts/irctc-booking-open.go
@@ -22,10 +22,10 @@ func main() {
                 fmt.Println ("Date (Ex: \"Aug 03, 2013\") should be given as argument")
                 os.Exit(-1)
         }
-        //According to irctc, we can book Aug 3rd 2013 ticket on June 2nd
+        //According to irctc, we can book Aug 11th 2013 ticket on June 12th
         //Lets figure out the duration
-        travelday := gettime_or_exit ("Aug 03, 2013")
-        bookingday := gettime_or_exit ("Jun 02, 2013")
+        travelday := gettime_or_exit ("Aug 11, 2013")
+        bookingday := gettime_or_exit ("Jun 12, 2013")
         timediff := travelday.Sub(bookingday)
 
         //Subtract this duration from the input to get the booking date
