669a3e8d001d6a6bdca31d991b74b59dbaf03dd0
Pranith Kumar K <pkarampu@redhat.com>
2014-05-25 18:02:28 +0530

    Script to run regressions on my vm
    Signed-off-by: Pranith Kumar K <pkarampu@redhat.com>

---
+++ scripts/regression-build.sh
@@ -0,0 +1,6 @@
+#/bin/bash -x
+H0="192.168.122.24"
+rm -f glusterfs-3.5qa2.tar.gz
+rm -rf ~/rpmbuild
+sudo make distclean;
+./autogen.sh && sudo ./configure --enable-fusermount && sudo make CFLAGS="-DDEBUG -g3 " install 1>/dev/null && make dist && rm -rf ~/rpmbuild && rpmbuild -ta glusterfs-3.5qa2.tar.gz && ssh root@$H0 "rm -rf /root/x86_64 && rpm -qa | grep gluster| xargs rpm -e; bash -x /root/dotfiles/scripts/kill_proc.sh" && scp -r ~/rpmbuild/RPMS/x86_64/ root@$H0: && ssh root@$H0 "rpm -ivh /root/x86_64/*rpm && sed -i '/H0=\${H0:=\`hostname --fqdn\`};/c\H0=192.168.122.24;' /usr/share/glusterfs/tests/include.rc"
