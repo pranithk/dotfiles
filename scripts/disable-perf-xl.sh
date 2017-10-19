d6e0d0429b21821ea16bace9f57e1563f514992a
Pranith Kumar K <pkarampu@redhat.com>
2017-10-19 16:57:20 +0530

    Add open-behind disabling to disable-perf-xl.sh
    Signed-off-by: Pranith Kumar K <pkarampu@redhat.com>

--- scripts/disable-perf-xl.sh
+++ scripts/disable-perf-xl.sh
@@ -4,3 +4,4 @@ gluster volume set $1 performance.io-cache off
 gluster volume set $1 performance.write-behind off
 gluster volume set $1 performance.stat-prefetch off
 gluster volume set $1 performance.read-ahead off
+gluster volume set $1 performance.open-behind off
