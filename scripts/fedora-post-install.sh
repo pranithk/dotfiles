1e90db707db2bcb3ef4d5a06a4b56d3f94e91d65
Pranith Kumar K <pkarampu@redhat.com>
2018-04-19 17:20:40 +0530

    Update glusterfs dependencies

--- scripts/fedora-post-install.sh
+++ scripts/fedora-post-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 #install glusterfs dev-tools
-dnf -y install git libtool autoconf automake flex bison openssl openssl-devel libibverbs-devel readline-devel libxml-devel libaio-devel libxml2-devel librdmacm librdmacm-devel
+dnf install automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config
 #install awesome vim
 dnf -y install vim-X11
 #install ruby
