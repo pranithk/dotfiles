479835a246dc04486f8934e2c67e9b983c7981de
Pranith Kumar K <pkarampu@redhat.com>
2018-04-20 11:14:40 +0530

    Add post-install script for fedora VM

---
+++ scripts/fedora-vm-post-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+#install glusterfs dev-tools
+dnf install automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config
+#install awesome vim
+dnf -y install vim-X11
+#install ruby
+dnf -y install ruby
+
+#install openssh-server
+dnf -y install openssh-server
+systemctl start sshd
+systemctl enable sshd
+
+#install cscope, valgrind
+dnf -y install cscope valgrind
+
+#install strace
+dnf -y install strace
