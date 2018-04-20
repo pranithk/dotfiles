2a578f878c7f6f74f27a95dff4095aa371f88d16
Pranith Kumar K <pkarampu@redhat.com>
2018-04-20 11:19:14 +0530

    Remove prompting for y/n for installing packages
    Signed-off-by: Pranith Kumar K <pkarampu@redhat.com>

--- scripts/fedora-post-install.sh
+++ scripts/fedora-post-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 #install glusterfs dev-tools
-dnf install automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config
+dnf -y install automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config
 #install awesome vim
 dnf -y install vim-X11
 #install ruby
